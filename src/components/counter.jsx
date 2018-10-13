d8a53e4b8d00945a16215e5fe13e417cb663a35c
Derek_Han <donglak.han@gmail.com>
2018-10-12 21:21:21 -0500

    add components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   handleIncrement = product => {
     console.log("product: ", product);
@@ -36,7 +23,6 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.renderTags()}
       </div>
     );
   }
